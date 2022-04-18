cbdb2cfc4154cdbcff2a67ae44444aba3ec0c14b
YuhanSu <88356987+YuhanSu-xjtu@users.noreply.github.com>
2022-04-18 16:09:27 +0800

    Add files via upload
    组合了收益和因子的对齐操作

--- YuhanLens/plotting.py
+++ YuhanLens/plotting.py
@@ -2,10 +2,11 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def plot_ic(dataset: pd.DataFrame, logger: bool = True):
+def plot_ic(dataset: pd.DataFrame, period: tuple = (3, 1), logger: bool = True):
     """
     计算因子IC序列，作图
     返回作图数据
+    :param period: 图表显示持仓时间和调仓周期
     :param logger: 日志输出
     :param dataset: merge_returns_factor的返回值
     :return: IC序列，IC平均值，IR
@@ -20,9 +21,9 @@ def plot_ic(dataset: pd.DataFrame, logger: bool = True):
     ic.rename(index="IC", inplace=True)
 
     ic.plot(kind="line")
-    plt.show()
+    plt.show(title="Period={},Hold={}".format(period[0], period[1]))
     ic.plot(kind="density")
-    plt.show()
+    plt.show(title="Period={},Hold={}".format(period[0], period[1]))
 
     ic_mean = ic.mean()
     ir = ic.mean() / ic.std()
@@ -35,10 +36,13 @@ def plot_ic(dataset: pd.DataFrame, logger: bool = True):
     return ic, ic_mean, ir
 
 
-def plot_quantile(dataset: pd.DataFrame, long_short: bool = False, compare: bool = False, logger: bool = True):
+def plot_quantile(dataset: pd.DataFrame, long_short: bool = False, compare: bool = False, period: tuple = (3, 1),
+                  logger: bool = True
+                  ):
     """
      因子分层和多空曲线
-    :param compare: 是否只比较最大和最小的净值曲线，当compare=True时，long_short函数无效
+    :param period: 图表显示持仓时间和调仓周期
+    :param compare: 是否只比较最大和最小的净值曲线，当compare=True时
     :param logger: 日志输出
     :param dataset: merge_returns_factor得到的处理好的因子和收益率数据集
     :param long_short:是否计算多空组合
@@ -51,6 +55,7 @@ def plot_quantile(dataset: pd.DataFrame, long_short: bool = False, compare: bool
     if long_short:
         dataset["TopBottom"] = dataset.iloc[:, len(dataset.columns) - 1] - dataset.iloc[:, 0]
         dataset["MidBottom"] = dataset.iloc[:, int(len(dataset.columns) / 2) - 1] - dataset.iloc[:, 0]
+        dataset["-1"] = - dataset.iloc[:, 0]
 
     dataset = dataset + 1
     net = dataset.cumprod()
@@ -59,8 +64,14 @@ def plot_quantile(dataset: pd.DataFrame, long_short: bool = False, compare: bool
     net = net.shift(1).fillna(1.0)
     net = net.append(end)
     if compare:
-        net = net.iloc[:, [0, net.shape[1] - 3]]
-    net.plot(kind="line")
+        # 如果compare=True, 多空、中空、bottom空头、bottom、top： 4条净值曲线
+        # 否则显示bottom、top：2条净值曲线
+        if long_short:
+            net = net.iloc[:, [0, net.shape[1] - 4, net.shape[1] - 3, net.shape[1] - 2, net.shape[1] - 1]]
+        else:
+            net = net.iloc[:, [0, net.shape[1] - 1]]
+
+    net.plot(kind="line", title="Period={},Hold={},Lag={}".format(period[0], period[1], period[2]))
     plt.show()
     if logger:
         print("------净值------")
--- YuhanLens/utils.py
+++ YuhanLens/utils.py
@@ -1,17 +1,45 @@
 import pandas as pd
 
 
-def compute_forward_returns(price: pd.DataFrame, periods: int = 1) -> pd.DataFrame():
+def compute_forward_lag_returns(price: pd.DataFrame, periods: int = 1, holding: int = 1,
+                                lag: int = 0) -> pd.DataFrame():
+    """
+   只接受股票的调仓日期的价格
+   因子日期和股票价格两者的日期需要对应
+   横轴为股票代码，纵轴为日期（必须是时间格式）
+   :param lag: 收益率滞后。当设置为0时，函数与compute_forward_returns等价
+   :param price: 股票价格数据
+   :param periods: 调仓周期
+   :param holding: 持仓时间
+   :return: 收益率数据
+    """
+    if lag + holding > periods:
+        raise ValueError("滞后期+持仓期不能大于调仓期，请重新设置参数")
+
+    returns = price.pct_change(holding).shift(-lag - holding)
+    returns = returns[::periods]
+
+    returns = returns.stack(dropna=True)
+    returns.index.set_names(names="stockcode", level=1, inplace=True)
+    returns.rename(index=str(periods) + "P", inplace=True)
+    return returns
+
+
+def compute_forward_returns(price: pd.DataFrame, periods: int = 1, holding: int = 1) -> pd.DataFrame():
     """
     只接受股票的调仓日期的价格
     因子日期和股票价格两者的日期需要对应
     横轴为股票代码，纵轴为日期（必须是时间格式）
     :param price: 股票价格数据
     :param periods: 调仓周期
+    :param holding: 持仓时间
     :return: 收益率数据
     """
-    returns = price.pct_change(periods).shift(-periods)
+    if holding > periods:
+        raise ValueError("持仓期不能大于调仓期，请重新设置参数")
+    returns = price.pct_change(holding).shift(-holding)
     returns = returns[::periods]
+
     returns = returns.stack(dropna=True)
     returns.index.set_names(names="stockcode", level=1, inplace=True)
     returns.rename(index=str(periods) + "P", inplace=True)
@@ -37,6 +65,7 @@ def get_clean_factor(factor: pd.Series, quantiles: int = 10, standard: bool = Fa
         return group
 
     bucket = factor.groupby("datetime").apply(quantile_calc, buckets=quantiles)
+
     bucket.rename(index="Group", inplace=True)
 
     if standard:
@@ -44,7 +73,8 @@ def get_clean_factor(factor: pd.Series, quantiles: int = 10, standard: bool = Fa
         factor = factor.groupby("datetime").apply(z_score)
         factor.rename(index=name, inplace=True)
 
-    factor = pd.merge(left=factor, right=bucket, left_index=True, right_index=True, how="left")
+    factor = pd.merge(left=factor, right=bucket, left_index=True, right_index=True, how="inner")
+    print(factor)
     return factor
 
 
@@ -55,7 +85,26 @@ def merge_returns_factor(forward_return: pd.DataFrame, clean_factor: pd.DataFram
     :param clean_factor: get_clean_factor函数处理后的因子
     :return: 处理好的因子和收益率数据
     """
-    merge_dataset = pd.merge(left=clean_factor, right=forward_return, left_index=True, right_index=True, how="left")
+    merge_dataset = pd.merge(left=clean_factor, right=forward_return, left_index=True, right_index=True, how="inner")
     merge_dataset.dropna(inplace=True)
 
+    return merge_dataset
+
+
+def get_merge_dataset(price: pd.DataFrame, factor: pd.Series, periods: int = 1, holding: int = 1,
+                      lag: int = 0, quantiles: int = 10, standard: bool = False) -> pd.DataFrame():
+    """
+    通过原始的价格和因子数据直接得出对齐的数据集
+    :param price: 股票价格数据
+    :param factor: 因子数据
+    :param periods: 调仓周期
+    :param holding: 持仓时间
+    :param lag: 收益率滞后。当设置为0时，函数与compute_forward_returns等价
+    :param quantiles: 分层的层数
+    :param standard: 是否标准化因子
+    :return:
+    """
+    returns = compute_forward_lag_returns(price=price, periods=periods, holding=holding, lag=lag)
+    factor = get_clean_factor(factor=factor, quantiles=quantiles, standard=standard)
+    merge_dataset = merge_returns_factor(forward_return=returns, clean_factor=factor)
     return merge_dataset
